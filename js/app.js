c21364f16222d3d8e861a7ab84a806afe16f192b
Ashyzan <info@studiocreativo69.it>
2024-03-08 15:09:06 +0100

    javascript: first approach: ask user name surname and color

---
+++ js/app.js
@@ -0,0 +1,21 @@
+console.log('prova di funzionamento')
+
+//chiedere all'utente il suo nome
+
+const nome = prompt('inserisci il tuo nome');
+console.log(nome);
+
+// chiudere allutent eil suo cognome
+
+const cognome = prompt('inserisci il tuo cognome');
+console.log(cognome);
+// chiedere all'utent eil suo colore preferito
+
+const colore = prompt('il tuo colore preferito?');
+console.log('colore');
+
+//generare una password con i valori inseriti dall'utente più un numero
+
+
+
+//BONUS: generare un numero intero random (tipo tra 0 e 99) usando la funzione Math.random() e le relative funzioni di arrotondamento

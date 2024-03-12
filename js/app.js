0842b0bb5ce8c453a7bb891bed5a3286279bf3b6
Ashyzan <info@studiocreativo69.it>
2024-03-12 16:54:14 +0100

    Password stamp console

--- js/app.js
+++ js/app.js
@@ -3,19 +3,25 @@ console.log('prova di funzionamento')
 //chiedere all'utente il suo nome
 
 const nome = prompt('inserisci il tuo nome');
-console.log(nome);
+console.log(nome); // string
 
-// chiudere allutent eil suo cognome
+// chiudere all' utente il suo cognome
 
 const cognome = prompt('inserisci il tuo cognome');
-console.log(cognome);
-// chiedere all'utent eil suo colore preferito
+console.log(cognome); // string
+// chiedere all'utente il suo colore preferito
 
 const colore = prompt('il tuo colore preferito?');
-console.log('colore');
+console.log(colore); // string
 
 //generare una password con i valori inseriti dall'utente più un numero
 
-
+console.log(nome + cognome + 21)
 
 //BONUS: generare un numero intero random (tipo tra 0 e 99) usando la funzione Math.random() e le relative funzioni di arrotondamento
+
+console.log(nome + cognome + Math.random() )
+
+// BONUS2 : il numero generato deve essere arrotondato a 2 decimali
+
+// console.log(nome+cognome+ Math.random()*2 )
